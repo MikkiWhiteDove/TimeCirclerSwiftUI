00436ed3f209b25a9c627d331e09d2128182aac8
Mishana <mishana@MacBook-Air-Mishana.local>
2022-02-16 13:29:11 +0300

    firstPush

---
+++ TimeCirculer/TimeCirculerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeCirculerApp.swift
+//  TimeCirculer
+//
+//  Created by Mishana on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimeCirculerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
